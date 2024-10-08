476739330b8ca6be51f387cf3a85e89467c56e2d
eunsung <edplab2019@gmail.com>
2024-10-08 16:09:38 +0900

    댓글 class 추가

--- musics/models.py
+++ musics/models.py
@@ -23,3 +23,12 @@ class Song(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.artist}"
+    
+class Comment(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE, related_name="comments")
+    # user = models.ForeignKey("users.User", on_delete = models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username}-{self.song.title}-{self.created_at}'
--- musics/views.py
+++ musics/views.py
@@ -1,20 +1,26 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.template import loader
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Song
 
 def index(request):
     latest_song_list = Song.objects.order_by("-release_date")[:5]
-    template = loader.get_template('musics/index.html')
-    
-    context = {'latest_song_list': latest_song_list}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'musics/index.html', {'latest_song_list': latest_song_list})
 
 
 def detail(request, song_id):
-    pass
+    try:
+        song = Song.objects.get(pk=song_id)
+    except Song.DoesNotExist:
+        raise Http404(f"Song [{song_id}] does not exist.")
+    return render(request, "musics/detail.html", {"song": song})
 def comments(request, song_id):
-    pass
+    song = get_object_or_404(Song, id = song_id)
+    comments = song.comments.all()
+    context = {
+        'song': song,
+        'comments': comments
+    }
+    return render(request, 'music/detail.html', context)
 
 def likes(request, song_id):
     pass
